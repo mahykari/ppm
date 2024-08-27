89b398f5a75d5bcfac53fe4f949ebf8ce536beca
mahykari <mahyar.karimi2079@gmail.com>
2024-08-27 14:41:43 +0200

    remove Group* exceptions

--- include/Exceptions.hh
+++ include/Exceptions.hh
@@ -1,13 +1,1 @@
 #include <stdexcept>
-
-class NoSuchGroupException : public std::runtime_error {
-public:
-  NoSuchGroupException()
-  : std::runtime_error("No such group exists") {}
-};
-
-class GroupAlreadyExistsException : public std::runtime_error {
-public:
-  GroupAlreadyExistsException()
-  : std::runtime_error("Group already exists") {}
-};
