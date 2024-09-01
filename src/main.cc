377f5c0bd6aee19753d697916fa8ba6edc1d0e39
mahykari <mahyar.karimi2079@gmail.com>
2024-09-01 15:59:08 +0200

    update main to add test for (Yao) garbling

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,11 @@
 #include "QuadraticResidueGroup.hh"
 #include "BigInt.hh"
+#include "Sha512YaoGarbler.hh"
 
 using namespace std;
 
 // TODO: integrate a testing framework.
-void test_qr() {
+void testQR() {
   mpz_t p;
   mpz_init(p);
   mpz_set_ui(p, 11);
@@ -33,6 +34,32 @@ void test_qr() {
   printf("Using a=9, b=3, a*b = %s\n", c_.toString().c_str());
 }
 
+void testGarbler() {
+  auto garbler = Sha512YaoGarbler();
+  LabelPair leftp = {"012", "345"};
+  LabelPair rightp = {"678", "9ab"};
+  LabelPair outp = {"cde", "f01"};
+
+  printf(
+    "Using leftp=[%s,%s], rightp=[%s,%s], outp=[%s,%s]:\n",
+    leftp[0].c_str(), leftp[1].c_str(),
+    rightp[0].c_str(), rightp[1].c_str(),
+    outp[0].c_str(), outp[1].c_str()
+  );
+
+  auto gate = garbler.enc(leftp, rightp, outp);
+  printf(
+    "- gate=[%s,%s,%s,%s]\n",
+    gate[0].c_str(), gate[1].c_str(),
+    gate[2].c_str(), gate[3].c_str()
+  );
+
+  auto label = garbler.dec(leftp[1], rightp[0], gate);
+  printf("- label=%s\n", label.c_str());
+}
+
 int main() {
-  test_qr();
+  testQR();
+  cout << string(80, '-') << '\n';
+  testGarbler();
 }
