b5fc12d6bd27a97cf4a93256bfd2c80c9306f812
mahykari <mahyar.karimi2079@gmail.com>
2024-10-04 17:44:01 +0200

    remove Element and impl. of BigInt, rename Utils to MathUtils
    Element no longer makes sense,
    as all groups are defined with template arguments.
    BigInt is now just a rename of mpz_class;
    so there is also no need to a .cc file for it.
    
    Beside Math Utils, we also have String Utils;
    so, it's good to keep them apart.

--- include/MathUtils.hh
+++ include/MathUtils.hh
@@ -2,7 +2,7 @@
 #define UTILS_HH
 
 #include <string>
-
+#include <gmp.h>
 
 const char HEX_ALPHABET[] = "0123456789abcdef";
 
@@ -10,5 +10,13 @@ uint8_t hexValue(char c);
 std::string hashSha512(const std::string& s);
 
 uint64_t timeBasedSeed();
+
+const size_t N_SAFEPRIMES = 256;
+// Sophie-Germain primes
+inline mpz_t SG_PRIMES[N_SAFEPRIMES + 5];
+// Safe primes
+inline mpz_t SAFE_PRIMES[N_SAFEPRIMES + 5];
+
+void initPrimes();
 
 #endif
