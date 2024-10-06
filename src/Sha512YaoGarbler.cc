5230bf7a5ed9905311fb9c4650689180252bf95c
mahykari <mahyar.karimi2079@gmail.com>
2024-10-06 14:34:14 +0200

    update Sha512YaoGarbler to use MathUtils, checksum
    Instead of the fixed 4-byte string,
    a redundancy check byte is added
    at the end of the plaintext.
    The resulting string is then 'encrypted'.

--- src/Sha512YaoGarbler.cc
+++ src/Sha512YaoGarbler.cc
@@ -1,14 +1,14 @@
 #include <cassert>
 #include <openssl/evp.h>
 #include "Exceptions.hh"
-#include "Utils.hh"
+#include "MathUtils.hh"
 #include "Sha512YaoGarbler.hh"
 
 // Output of SHA-512 is 128 characters long,
 // when represented in hexadecimal.
 const size_t SHA512_HEX_SIZE = EVP_MAX_MD_SIZE * 2;
-const std::string CHECK_HEX_STRING = "abcd";
-const size_t CHECK_HEX_SIZE = CHECK_HEX_STRING.size();
+// The last character is an XOR checksum.
+const size_t CHECK_HEX_SIZE = 1;
 
 bool Sha512YaoGarbler::checkLabels(std::vector<Label> labels) {
   bool valid = true;
@@ -25,7 +25,12 @@ Ciphertext Sha512YaoGarbler::encImpl(
 {
   auto s = left + right;
   auto h = hashSha512(s);
-  auto out1 = out + CHECK_HEX_STRING;
+
+  uint8_t check = 0;
+  for (size_t i = 0; i < out.size(); i++)
+    check ^= hexValue(out[i]);
+  auto out1 = out + HEX_ALPHABET[check];
+
   Ciphertext cipher(out1.size(), 0);
   for (size_t i = 0; i < out1.size(); i++)
     cipher[i] = HEX_ALPHABET[hexValue(out1[i]) ^ hexValue(h[i])];
@@ -41,7 +46,12 @@ Label Sha512YaoGarbler::decImpl(
   Label label(size, 0);
   for (size_t i = 0; i < size; i++)
     label[i] = HEX_ALPHABET[hexValue(cipher[i]) ^ hexValue(h[i])];
-  if (label.substr(size - CHECK_HEX_SIZE) != CHECK_HEX_STRING)
+
+  uint8_t check = 0;
+  for (size_t i = 0; i < size - 1; i++)
+    check ^= hexValue(label[i]);
+
+  if (label[size - 1] != HEX_ALPHABET[check])
     throw InvalidCipher();
-  return label.substr(0, size - CHECK_HEX_SIZE);
+  return label.substr(0, size - 1);
 }
