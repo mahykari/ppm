89e0d45a1cd6fbd04dea63b20e9dc604f7be6087
mahykari <mahyar.karimi2079@gmail.com>
2024-10-06 14:38:07 +0200

    renamed main to Test, updated to use BigInt

--- src/Test.cc
+++ src/Test.cc
@@ -1,37 +1,33 @@
+#include <cassert>
+#include <iostream>
 #include "QuadraticResidueGroup.hh"
 #include "BigInt.hh"
 #include "Sha512YaoGarbler.hh"
+#include "StringUtils.hh"
 
 using namespace std;
 
 // TODO: integrate a testing framework.
 void testQR() {
-  mpz_t p;
-  mpz_init(p);
-  mpz_set_ui(p, 11);
+  BigInt p(11);
   cout << "Using prime modulus p="
-    <<  mpz_get_str(NULL, 10, p) << '\n';
+    <<  toString(p) << '\n';
   auto group = QuadraticResidueGroup(p);
-  cout << "Using " << group.toString() << '\n';
+  cout << "Using " << toString(group) << '\n';
   auto g = group.randomGenerator();
-  printf("Using generator g = %s\n", g.toString().c_str());
-  
+  cout << "Using generator g = " << toString(g) << '\n';
+
   for (int i = 1; i <= 5; i++) {
-    mpz_t exp;
-    mpz_init_set_ui(exp, i);
-    auto h = group.exp(g, exp);
-    printf("g^%d = %s\n", i, h.toString().c_str());
-  } 
-
-  mpz_t a, b, result;
-  mpz_inits(a, b, result, NULL);
-  mpz_set_ui(a, 9);
-  mpz_set_ui(b, 3);
-
-  auto a_ = BigInt(a);
-  auto b_ = BigInt(b);
-  auto c_ = group.mul(a, b);
-  printf("Using a=9, b=3, a*b = %s\n", c_.toString().c_str());
+    BigInt e(i);
+    auto h = group.exp(g, e);
+    printf("g^%d = %s\n", i, toString(h).c_str());
+  }
+  BigInt a(9);
+  BigInt b(3);
+  BigInt result = group.mul(a, b);
+  printf(
+    "Using a=9, b=3, a*b = %s\n",
+    toString(result).c_str());
 }
 
 void testGarbler() {
@@ -47,19 +43,22 @@ void testGarbler() {
     outp[0].c_str(), outp[1].c_str()
   );
 
-  auto gate = garbler.enc(leftp, rightp, outp);
+  auto gate =
+    garbler.enc(leftp, rightp, outp);
   printf(
     "- gate=[%s,%s,%s,%s]\n",
     gate[0].c_str(), gate[1].c_str(),
     gate[2].c_str(), gate[3].c_str()
   );
 
-  auto label = garbler.dec(leftp[1], rightp[0], gate);
+  auto label =
+    garbler.dec(leftp[1], rightp[0], gate);
+  assert(label == outp[1]);
   printf("- label=%s\n", label.c_str());
 }
 
 int main() {
   testQR();
-  cout << string(80, '-') << '\n';
+  cout << string(70, '-') << '\n';
   testGarbler();
 }
