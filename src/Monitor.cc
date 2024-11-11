a31ad07d5adbb412fc8bed9babb597fe0e8a5172
mahykari <mahyar.karimi2079@gmail.com>
2024-11-11 15:57:25 +0100

    update Monitor, System

--- src/Monitor.cc
+++ src/Monitor.cc
@@ -19,34 +19,47 @@ int main (int argc, char *argv[]) {
   SetUp();
   auto messageHandler = MessageHandler(L::SYSTEM_PORT, L::MONITOR_PORT);
 
-  unsigned securityParameter = 80;
+  unsigned securityParameter = 240;
   auto primeModulus = BigInt(SAFE_PRIMES[securityParameter]);
   printf("I: using prime modulus %s\n", primeModulus.get_str(10).c_str());
 
+  unsigned monitorStateLength = 1;
+  // CAUTION: systemStateLength should be a perfect square.
+  unsigned systemStateLength = (1 << 10);
+  unsigned inputLength = monitorStateLength + systemStateLength;
+  unsigned outputLength = monitorStateLength + 1;
+  auto circuit = Circuit(inputLength, outputLength);
+  std::vector<unsigned> gateIds;
+  for (unsigned i = 0; i < systemStateLength; i++)
+    gateIds.push_back(monitorStateLength + i);
+  unsigned n = gateIds.size();
+  // printf("D: n = %u\n", n);
+  for (unsigned i = 0; i < n - 1; i++) {
+    unsigned inputLeft = gateIds[2*i];
+    unsigned inputRight = gateIds[2*i + 1];
+    gateIds.push_back(circuit.addGate(inputLeft, inputRight));
+  }
+  // Last gate computes SUM(x_{2i} x_{2i + 1}).
+  auto sumGateId = gateIds.back();
+  // ~ (m SUM(x_{2i} x_{2i + 1}))
+  auto notFlagGateId = circuit.addGate(0, sumGateId);
+  circuit.addGate(0, 0); // ~ m
+  circuit.addGate(notFlagGateId, notFlagGateId); // m SUM(x_{2i} x_{2i + 1})
+  auto monitorMemory = L::MonitorMemory {
+    .circuit = &circuit
+  };
+
+  printf(
+    "D: circuit size = %u, input length: %u\n",
+    circuit.size(), inputLength);
+
   auto parameters = L::ParameterSet {
-    // .gateCount = 3,
-    // .monitorStateLength = 0,
-    .gateCount = 6,
-    .monitorStateLength = 1,
-    .systemStateLength = 4,
+    .gateCount = circuit.size() - inputLength,
+    .monitorStateLength = monitorStateLength,
+    .systemStateLength = systemStateLength,
     .group = QuadraticResidueGroup(primeModulus),
     .garbler = Sha512YaoGarbler(),
     .securityParameter = securityParameter
-  };
-
-  // auto circuit = Circuit(4, 1);
-  // circuit.addGate(0, 1);
-  // circuit.addGate(2, 3);
-  // circuit.addGate(4, 5);
-  auto circuit = Circuit(5, 2);
-  circuit.addGate(1, 2); //  5: ~ (x0 x1)
-  circuit.addGate(3, 4); //  6: ~ (x2 x3)
-  circuit.addGate(5, 6); //  7: x0 x1 + x2 x3
-  circuit.addGate(0, 7); //  8: ~ (m (x0 x1 + x2 x3))
-  circuit.addGate(0, 0); //  9: ~ m
-  circuit.addGate(8, 8); // 10: m (x0 x1 + x2 x3)
-  auto monitorMemory = L::MonitorMemory {
-    .circuit = &circuit
   };
 
   auto interface = L::MonitorInterface(
@@ -54,18 +67,18 @@ int main (int argc, char *argv[]) {
 
   interface.run();
 
-  std::cout << std::string(80, '=') << '\n';
-  std::cout << "TESTING BELLARE-MICALI OT PROTOCOL" << '\n';
-  std::cout << std::string(80, '=') << '\n';
+  // std::cout << std::string(80, '=') << '\n';
+  // std::cout << "TESTING BELLARE-MICALI OT PROTOCOL" << '\n';
+  // std::cout << std::string(80, '=') << '\n';
 
-  auto parameters1 = B::ParameterSet {
-    .group = QuadraticResidueGroup(primeModulus)
-  };
+  // auto parameters1 = B::ParameterSet {
+  //   .group = QuadraticResidueGroup(primeModulus)
+  // };
 
-  auto chooserMemory = B::ChooserMemory { .sigma = 1 };
-  auto interface1 = B::ChooserInterface(
-    &parameters1, &chooserMemory, &messageHandler);
-  interface1.run();
+  // auto chooserMemory = B::ChooserMemory { .sigma = 1 };
+  // auto interface1 = B::ChooserInterface(
+  //   &parameters1, &chooserMemory, &messageHandler);
+  // interface1.run();
 
   exit(EXIT_SUCCESS);
 }
--- src/System.cc
+++ src/System.cc
@@ -27,50 +27,72 @@ public:
   }
 };
 
+class JumpSweepSystem : public MonitorableSystem {
+public:
+  JumpSweepSystem(unsigned N) : N(N) {
+    x = std::vector<bool> (N, 0);
+  }
+
+  unsigned N;
+  unsigned cntr = 0;
+  std::vector<bool> x;
+  void next() override {
+    x[cntr] = 1 - x[cntr];
+    cntr += 2;
+    if (cntr == N)
+      cntr = 1;
+    if (cntr == N + 1)
+      cntr = 0;
+  }
+  const std::vector<bool>& data() override {
+    return x;
+  }
+};
+
 int main() {
   SetUp();
   auto messageHandler = MessageHandler(L::MONITOR_PORT, L::SYSTEM_PORT);
 
-  unsigned securityParameter = 80;
+  unsigned securityParameter = 240;
   auto primeModulus = BigInt(SAFE_PRIMES[securityParameter]);
   printf("I: using prime modulus %s\n", primeModulus.get_str(10).c_str());
+  unsigned monitorStateLength = 1;
+  unsigned systemStateLength = (1 << 10);
   auto parameters = L::ParameterSet {
-    // .gateCount = 3,
-    // .monitorStateLength = 0,
-    .gateCount = 6,
-    .monitorStateLength = 1,
-    .systemStateLength = 4,
+    .gateCount = systemStateLength + 2,
+    .monitorStateLength = monitorStateLength,
+    .systemStateLength = systemStateLength,
     .group = QuadraticResidueGroup(primeModulus),
     .garbler = Sha512YaoGarbler(),
     .securityParameter = securityParameter
   };
 
-  auto system = SweepSystem();
+  auto system = JumpSweepSystem(systemStateLength);
   auto systemMemory = L::SystemMemory {
     .system = &system
   };
 
   auto interface = L::SystemInterface(
     &parameters, &systemMemory, &messageHandler);
-  printf("D: running system interface\n");
+  // printf("D: running system interface\n");
   interface.run();
 
-  std::cout << std::string(80, '=') << '\n';
-  std::cout << "TESTING BELLARE-MICALI OT PROTOCOL" << '\n';
-  std::cout << std::string(80, '=') << '\n';
+  // std::cout << std::string(80, '=') << '\n';
+  // std::cout << "TESTING BELLARE-MICALI OT PROTOCOL" << '\n';
+  // std::cout << std::string(80, '=') << '\n';
 
-  auto parameters1 = B::ParameterSet {
-    .group = QuadraticResidueGroup(primeModulus)
-  };
+  // auto parameters1 = B::ParameterSet {
+  //   .group = QuadraticResidueGroup(primeModulus)
+  // };
 
-  auto senderMemory = B::SenderMemory {
-    .messages = {"12345", "67890"},
-  };
+  // auto senderMemory = B::SenderMemory {
+  //   .messages = {"12345", "67890"},
+  // };
 
-  auto interface1 = B::SenderInterface(
-    &parameters1, &senderMemory, &messageHandler);
+  // auto interface1 = B::SenderInterface(
+  //   &parameters1, &senderMemory, &messageHandler);
 
-  interface1.run();
+  // interface1.run();
 
   exit(EXIT_SUCCESS);
 }
