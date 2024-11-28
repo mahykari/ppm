cc1d6ec2e8bb08896951114d6668ee9284eebff4
mahykari <mahyar.karimi2079@gmail.com>
2024-11-28 13:08:53 +0100

    add exception for Module

--- include/Exceptions.hh
+++ include/Exceptions.hh
@@ -1,3 +1,6 @@
+#ifndef EXCEPTIONS_HH
+#define EXCEPTIONS_HH
+
 #include <stdexcept>
 
 class InvalidLabels : public std::runtime_error {
@@ -17,3 +20,11 @@ public:
   NonSendStateHasNoMessage()
     : std::runtime_error("Non-send state has no message") {}
 };
+
+class UnbuiltModuleHasNoOutput : public std::runtime_error {
+public:
+  UnbuiltModuleHasNoOutput()
+    : std::runtime_error("Unbuilt module has no output") {}
+};
+
+#endif
