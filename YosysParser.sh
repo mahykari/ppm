8241dcd86a8c191232f1a3c2f51fa72cb72ebbea
mahykari <mahyar.karimi2079@gmail.com>
2025-01-09 14:43:53 +0100

    update YosysParser to cond. skip synthesis
    This is significant in experiments.
    Once the circuit is built,
    we can change security parameter independently.
    So there's no need for synthesis on every iteration.

--- YosysParser.sh
+++ YosysParser.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 source "$HOME/oss-cad-suite/environment"
 
+if [ -f ./synth.blif ]; then
+  echo "skipping synthesis, BLIF file already exists."
+fi
+
 if [ "$#" -ne 1 ]; then
-   echo "Usage: $0 verilog_file" >&2
-   exit 1
+  echo "Usage: $0 verilog_file" >&2
+  exit 1
 fi
 
 # Create new script with substitution
