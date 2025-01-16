35bd3beefe4d0e805071d17751fb6af70ff1d755
mahykari <mahyar.karimi2079@gmail.com>
2025-01-16 17:31:04 +0100

    update StringUtils to add random hex generator

--- include/StringUtils.hh
+++ include/StringUtils.hh
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <tuple>
+#include <vector>
+#include <fstream>
+
 #include "BigInt.hh"
 #include "QuadraticResidueGroup.hh"
 #include "YaoGarbler.hh"
@@ -18,5 +21,23 @@ readBigInts(const std::string& message, int base, int count);
 
 std::tuple<std::vector<GarbledGate>, std::string>
 readGarbledGates(const std::string& message, int count);
+
+std::tuple<std::vector<std::string>, std::string>
+readStrings(const std::string& message, int count);
+
+// A random hex string of specified length.
+// This string is generated using /dev/urandom.
+struct HexGeneratorState {
+  std::ifstream urandom;
+  std::vector<unsigned char> buffer;
+  size_t bufferPos;
+
+  HexGeneratorState();
+  ~HexGeneratorState();
+};
+
+extern HexGeneratorState SingletonHexGeneratorState;
+
+std::string randomHexString(unsigned length);
 
 #endif
