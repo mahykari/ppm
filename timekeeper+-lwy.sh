f7382cf55b1311b0707511b8ad3778ed4a9c0992
mahykari <mahyar.karimi2079@gmail.com>
2025-01-24 16:04:37 +0100

    update logs for timekeeper+-lwy

--- timekeeper+-lwy.sh
+++ timekeeper+-lwy.sh
@@ -31,35 +31,35 @@ mkdir -p logs
 
 # Run the protocol with the timekeeper spec and system.
 # The sets represent n_in (number of internal doors) and wordlen respectively.
-N_EX=30
-for comb in {10,30,100}_{8,16,32}; do
-  IFS='_' read -r n_in w <<< "$comb"
-  printf "I: parameters n_ex=%2d, n_in=%3d, wordlen=%2d\n" $N_EX $n_in $w
 
-  for s in {128,256,768,1024,1536,2048,3072}; do
+for comb in {10,30}_{0,10,30,100}_{16,32}; do
+  IFS='_' read -r n_ex n_in w <<< "$comb"
+  printf "I: parameters n_ex=%2d, n_in=%3d, wordlen=%2d\n" $n_ex $n_in $w
+
+  for s in {768,1024,1536,2048,3072}; do
     printf "I:   security=%4d\n" $s
     # In case we want to skip this iteration,
     # both log files should exist.
-    if [ -f "./logs/monitor_$N_EX-$n_in-$w-$s.log" ] && \
-      [ -f "./logs/system_$N_EX-$n_in-$w-$s.log" ]
+    if [ -f "./logs/monitor_$n_ex-$n_in-$w-$s.log" ] && \
+      [ -f "./logs/system_$n_ex-$n_in-$w-$s.log" ]
     then
       printf "I:  skipping iteration; logs already exist...\n"
       continue
     fi
     # Preparing the synthesis script with the new parameters.
-    sed "s/SPEC_FILE/-D N_EX=$N_EX -D N_IN=$n_in -D WORDLEN=$w SPEC_FILE/g" \
+    sed "s/SPEC_FILE/-D N_EX=$n_ex -D N_IN=$n_in -D WORDLEN=$w SPEC_FILE/g" \
     synth.ys.bak > synth.ys
 
     mslen=$((w * 2))
-    sslen=$(( 4 * w * (N_EX + n_in) ))
+    sslen=$(( 4 * w * (n_ex + n_in) ))
     # Calling Monitor and System with the current combination
     ./Monitor -proto lwy -security $s -mslen $mslen -sslen $sslen \
-      -spec timekeeper+.v -wordlen $w -nex $N_EX -nin $n_in \
-      > ./logs/monitor_$N_EX-$n_in-$w-$s.log 2>&1 &
+      -spec timekeeper+.v -wordlen $w -nex $n_ex -nin $n_in \
+      > ./logs/monitor_$n_ex-$n_in-$w-$s.log 2>&1 &
     mpid=$!
     ./System -proto lwy -security $s -mslen $mslen -sslen $sslen \
-      -sys timekeeper+ -wordlen $w -nex $N_EX -nin $n_in \
-      > ./logs/system_$N_EX-$n_in-$w-$s.log 2>&1 &
+      -sys timekeeper+ -wordlen $w -nex $n_ex -nin $n_in \
+      > ./logs/system_$n_ex-$n_in-$w-$s.log 2>&1 &
     spid=$!
 
     # Waiting for the processes to finish
